5dc2d71915914c2a1f0e61b584a302e6780372d7
whirlun <whirlun@yahoo.co.jp>
2025-01-13 13:44:57 -0800

    use webview instead of rendering in flutter widgets

--- lib/src/platforms/macos/text_reader.dart
+++ lib/src/platforms/macos/text_reader.dart
@@ -1,50 +1,85 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
+import 'package:webview_flutter/webview_flutter.dart';
+class TextReader extends StatefulWidget {
 
-class TextReader extends StatelessWidget {
-  const TextReader({super.key});
+  TextReader({super.key});
 
-  final String text = """
-  <div class='container'>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget commodo nulla. Ut sollicitudin, ligula at ultrices rhoncus, felis diam pretium ante, eu auctor arcu massa at metus. In ut tristique justo. Nulla commodo ultrices velit vel ultricies. Etiam nec velit purus. Etiam vehicula lectus ante, ac luctus eros aliquet non. Nullam faucibus vel risus nec sagittis. Phasellus sed finibus sem. Maecenas ultrices velit lorem, vel elementum mi commodo ac. Sed vel odio vitae nulla hendrerit eleifend. Praesent lacinia velit eget commodo faucibus.</p>
+  @override
+  State<StatefulWidget> createState() {
+    return _TextReaderState();
+  }
+}
 
-<p>Sed sed nunc libero. Sed semper facilisis mauris vitae tincidunt. Donec non pretium odio, sed feugiat nulla. Integer mauris ante, molestie et feugiat ut, porta at neque. Praesent eu enim ipsum. Donec urna neque, porta ac vulputate at, ullamcorper ut nibh. Nullam accumsan rhoncus ligula vel accumsan. Suspendisse et mi et libero placerat faucibus. Maecenas justo odio, tempor sit amet lacinia eget, ullamcorper vel ex. Aenean nunc ipsum, ultrices id arcu quis, euismod lacinia purus. In eget scelerisque risus. Nunc faucibus rhoncus porttitor. Fusce quam lorem, fringilla non tincidunt viverra, aliquet sed tortor.</p>
+class _TextReaderState extends State<TextReader> {
 
-</div>
-    """;
+  late final WebViewController _controller;
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.disabled)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadHtmlString("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Two Column Lorem Ipsum</title>
+  <style>
+    .two-columns {
+      column-count: 2;
+      column-gap: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="two-columns">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</body>
+</html>
+    """);
+
+}
 
   @override
   Widget build(BuildContext context) {
-    return SelectableRegion(
-        focusNode: FocusNode(),
-        selectionControls: MaterialTextSelectionControls(),
-        child: Row(children: [
-          Expanded(
-              child: Html(
-            data: text,
-            style: {
-              "p": Style(
-                fontSize: FontSize(16),
-                color: Color(0xFF459999),
-                fontFamily: 'Arial',
-              ),
-            },
-          )),
-          Expanded(
-              child: Html(
-            data: text,
-            style: {
-              "p": Style(
-                fontSize: FontSize(16),
-                color: Color(0xFF000000),
-                fontFamily: 'Arial',
-              ),
-            },
-          ))
-        ]));
+    return LayoutBuilder(builder: (context, constraints) {
+      final width = constraints.maxWidth;
+      final height = constraints.maxHeight;
+      print("$height, $width");
+      return SizedBox(width: width, height: height, child: WebViewWidget(controller: _controller));
+    });
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import macos_window_utils
 import objectbox_flutter_libs
 import path_provider_foundation
 import screen_retriever_macos
+import webview_flutter_wkwebview
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -20,5 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ObjectboxFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "ObjectboxFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
